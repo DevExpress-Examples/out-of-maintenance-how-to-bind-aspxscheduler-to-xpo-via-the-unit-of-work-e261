3a0ca89f325bdcdcbc8dd9aa2c958fec544924f9
mmakarova <makarova.marina@corp.devexpress.com>
2022-08-11 18:04:43 +0400

    Fix VB

---
+++ VB/WebSite/App_Code/XPObjects.vb
@@ -0,0 +1,42 @@
+﻿Imports System
+Imports DevExpress.Xpo
+
+Public Class Task
+	Inherits XPObject
+
+	Public Sub New(ByVal session As Session)
+		MyBase.New(session)
+	End Sub
+	Public AllDay As Boolean ' Appointment.AllDay
+
+	<Size(SizeAttribute.Unlimited)>
+	Public Description As String ' Appointment.Description -  !! To set the Memo field type.
+
+	Public Finish As Date ' Appointment.End
+	Public Label As Integer ' Appointment.Label
+	Public Location As String ' Appointment.Location
+
+	<Size(SizeAttribute.Unlimited)>
+	Public Recurrence As String ' Appointment.RecurrenceInfo -  !! To set the Memo field type.
+
+	<Size(SizeAttribute.Unlimited)>
+	Public Reminder As String ' Appointment.ReminderInfo -  !! To set the Memo field type.
+
+	Public Created As Date ' Appointment.Start
+	Public Status As Integer ' Appointment.Status
+	Public Subject As String ' Appointment.Subject
+	Public AppointmentType As Integer ' Appointment.Type
+	Public Employee As Employee
+End Class
+
+Public Class Employee
+	Inherits XPObject
+
+	Public Sub New(ByVal session As Session)
+		MyBase.New(session)
+	End Sub
+
+	<Size(SizeAttribute.Unlimited)>
+	Public Name As String ' Resource.Caption -  !! To set the Memo field type.
+End Class
+
